099dd8b91e3941b43f56c5981b695f91b14b18b1
lim <lbooks424@gmail.com>
2023-11-10 14:31:11 +0900

    [add] case to handle the P2025 error code, which occurs when a record is not found in the database

--- src/prisma-client-exception/prisma-client-exception.filter.ts
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -20,6 +20,14 @@ export class PrismaClientExceptionFilter extends BaseExceptionFilter {
         });
         break;
       }
+      case 'P2025' : {
+        const status = HttpStatus.NOT_FOUND;
+        response.status(status).json({
+          statusCode: status,
+          message: message,
+        });
+        break;
+      }
       default:
         super.catch(exception, host);
         break;
